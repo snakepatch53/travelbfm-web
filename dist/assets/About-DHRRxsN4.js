fa49afc10d96dc590ee70eda5e9a38f025096b34
snakepatch53 <snakepatch53@gmail.com>
2024-02-23 21:41:29 -0500

    On branch master Your branch is up to date with 'origin/master'.
    Changes to be committed:
    	new file:   dist/assets/About-CEJ-F4Yc.js
    	deleted:    dist/assets/About-DHRRxsN4.js
    	renamed:    dist/assets/Banner-CWQeGWs6.js -> dist/assets/Banner-Cx04o_SH.js
    	renamed:    dist/assets/Business-vi6N0ku2.js -> dist/assets/Business-B8Kyg6LV.js
    	renamed:    dist/assets/Business-DFWFciP2.js -> dist/assets/Business-CNogyt6x.js
    	new file:   dist/assets/Categories-C-zNRT_T.js
    	deleted:    dist/assets/Categories-CSdLUDJM.js
    	renamed:    dist/assets/Contact-BuM2wwDB.js -> dist/assets/Contact-B_Xq6t2L.js
    	renamed:    dist/assets/CrudBackground-CH5sytJb.js -> dist/assets/CrudBackground-CjO61FQl.js
    	renamed:    dist/assets/Form-CaR2LGWh.js -> dist/assets/Form-HdUU5nxD.js
    	renamed:    dist/assets/History-D47oJP5C.js -> dist/assets/History-Co2CUdo4.js
    	renamed:    dist/assets/Home-CaYy4wBv.js -> dist/assets/Home-Bp3ESFwu.js
    	renamed:    dist/assets/Home-CesZ_THK.js -> dist/assets/Home-yudA6goh.js
    	deleted:    dist/assets/Login-BB9wuDbx.js
    	new file:   dist/assets/Login-CLzVtqhK.js
    	renamed:    dist/assets/ModalProducts-DThmIq_k.js -> dist/assets/ModalProducts-CLSkgOZ9.js
    	renamed:    dist/assets/PartnerItem-BpPk7qnC.js -> dist/assets/PartnerItem-_49pDnxJ.js
    	renamed:    dist/assets/Products-CUYBq8zs.js -> dist/assets/Products-Cqe5NJnC.js
    	renamed:    dist/assets/Profile-DR8Be53M.js -> dist/assets/Profile-X_jctDPf.js
    	renamed:    dist/assets/Register-BAavI2SO.js -> dist/assets/Register-KR36-fc0.js
    	renamed:    dist/assets/Shop-BTybjoLh.js -> dist/assets/Shop-DmlApv9l.js
    	renamed:    dist/assets/Title-EjxELi7N.js -> dist/assets/Title-D_SkDEiC.js
    	renamed:    dist/assets/Users-DZ8RLMfm.js -> dist/assets/Users-CME3JE2A.js
    	renamed:    dist/assets/businesses-B-VnBIup.js -> dist/assets/businesses-BZQ6S_Ws.js
    	renamed:    dist/assets/categories-v0uONWXX.js -> dist/assets/categories-CA5ysTkW.js
    	renamed:    dist/assets/effect-fade-BkwBmW0y.js -> dist/assets/effect-fade-tP3CsaFe.js
    	renamed:    dist/assets/index-DPwzPAO1.js -> dist/assets/index-BlHX8wUy.js
    	renamed:    dist/assets/index.esm-t_k-gWDQ.js -> dist/assets/index.esm-DxlewJgA.js
    	renamed:    dist/assets/products-BI_ipci7.js -> dist/assets/products-CwVT3N1j.js
    	renamed:    dist/assets/useCrudPanel-BOgscc5u.js -> dist/assets/useCrudPanel-CYpB8VXP.js
    	modified:   dist/index.html

--- dist/assets/About-DHRRxsN4.js
+++
@@ -1,1 +0,0 @@
-import{j as t,P as o}from"./index-DPwzPAO1.js";import{B as r}from"./Banner-CWQeGWs6.js";import{H as s}from"./History-D47oJP5C.js";import"./Title-EjxELi7N.js";function m(){return t.jsxs(o,{children:[t.jsx(r,{title:"Acerca de Nosotros"}),t.jsx("section",{children:t.jsx(s,{})})]})}export{m as default};
